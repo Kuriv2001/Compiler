891eeeb555eb8e25efead0616b4d5bb1d617c18e
Kuriv <gregory.richard@epfl.ch>
2024-03-01 13:24:24 +0100

    added stuff

--- lab01_interpreter/src/main/scala/alpine/evaluation/RuntimeError.scala
+++
@@ -1,9 +0,0 @@
-package alpine
-package evaluation
-
-import alpine.Diagnostic
-
-/** An error that occurred during run-time evaluation. */
-final class RuntimeError(
-    val summary: String, site: SourceSpan
-) extends Diagnostic(Diagnostic.Level.Error, site)
