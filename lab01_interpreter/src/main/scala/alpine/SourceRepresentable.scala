cc4ce0e88092b836afc505fd64c1a6c88501abcc
Antoine Garin <antoine.garin@epfl.ch>
2024-04-06 20:11:50 +0200

    finished initialization of lab5

--- lab01_interpreter/src/main/scala/alpine/SourceRepresentable.scala
+++
@@ -1,9 +0,0 @@
-package alpine
-
-/** A value that has a source-level representation. */
-trait SourceRepresentable:
-
-  /** The site from which `this` was parsed. */
-  def site: SourceSpan
-
-end SourceRepresentable
